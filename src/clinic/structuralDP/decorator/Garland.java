e360c683113c0a3a3e48fbb44779361f8c5336f9
mahdihatami <iz.hatami@gmail.com>
2019-07-18 11:45:07 +0300

    impl decorator dp

---
+++ src/clinic/structuralDP/decorator/Garland.java
@@ -0,0 +1,20 @@
+package clinic.structuralDP.decorator;
+
+public class Garland extends TreeDecorator
+{
+    public Garland(ChristmasTree tree)
+    {
+        super(tree);
+    }
+
+    @Override
+    public String decorate()
+    {
+        return super.decorate() + decorateWithGarland();
+    }
+
+    private String decorateWithGarland()
+    {
+        return " with Garland";
+    }
+}
